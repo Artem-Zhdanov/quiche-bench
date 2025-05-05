0bf2c29bf657adee690b4bedaebb7188ba9d727a
Artem Zhdanov <artem.a.zhdanov@gmail.com>
2025-05-05 12:44:40 +0000

    Cleanup

--- src/publisher.rs
+++ src/publisher.rs
@@ -2,21 +2,19 @@ use crate::config::BLOCK_SIZE;
 use crate::quic_config::configure_client;
 use crate::{MAGIC_NUMBER, flush_send, now_ms};
 use anyhow::{Result, bail};
+use quiche::{ConnectionId, RecvInfo};
 use ring::rand::{SecureRandom, SystemRandom};
 use std::io;
 use std::net::SocketAddr;
+use std::time::Duration;
 use tokio::net::UdpSocket;
 use tokio::time::{Instant, sleep_until};
 
-use std::time::Duration;
-
-use quiche::{ConnectionId, RecvInfo};
-
 const ESTABLISH_CONNECTION_TIMEOUT: Duration = Duration::from_secs(5);
+const MAX_MESSAGE_NUM: u32 = 100000;
 
 pub async fn run(addr: String, port: u16) -> Result<()> {
     let mut data_to_send = vec![42u8; BLOCK_SIZE];
-    const MAX_MESSAGE_NUM: u32 = 100000;
 
     let rng = SystemRandom::new();
 
@@ -37,11 +35,9 @@ pub async fn run(addr: String, port: u16) -> Result<()> {
     // socket.set_nonblocking(true)?;
 
     let mut conn = quiche::connect(None, &scid, socket.local_addr()?, peer, &mut config)?;
-
     flush_send!(conn, socket, write_buf, peer);
 
     let start = Instant::now();
-    let mut connection_established = false;
 
     let mut stream_id: Option<u64> = None;
     let mut message_count = 0;
@@ -50,7 +46,7 @@ pub async fn run(addr: String, port: u16) -> Result<()> {
 
     while !conn.is_closed() {
         // Check that connection was established during last ESTABLISH_CONNECTION_TIMEOUT
-        if start.elapsed() > ESTABLISH_CONNECTION_TIMEOUT && !connection_established {
+        if start.elapsed() > ESTABLISH_CONNECTION_TIMEOUT && stream_id.is_none() {
             anyhow::bail!(
                 "Can't establish connection in {:?}",
                 ESTABLISH_CONNECTION_TIMEOUT
@@ -59,187 +55,77 @@ pub async fn run(addr: String, port: u16) -> Result<()> {
 
         match socket.recv_from(&mut read_buf).await {
             Ok((len, from)) => {
-                if let Err(err) = conn.recv(
-                    &mut read_buf[..len],
-                    RecvInfo {
-                        from,
-                        to: socket.local_addr()?,
-                    },
-                ) {
-                    bail!("Error passing packet to Quic {:?}", err);
-                }
+                let info = RecvInfo {
+                    from,
+                    to: socket.local_addr()?,
+                };
+                conn.recv(&mut read_buf[..len], info)?;
             }
-            Err(e) => {
-                if e.kind() == io::ErrorKind::WouldBlock {
-                    tokio::task::yield_now().await;
-                    // Ok, no data
-                } else {
-                    tracing::error!("Error reading packet from socket: {:?}", e);
-                }
-            }
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
+            Err(e) => tracing::error!("Error reading packet from socket: {e}"),
         }
-
-        // Read from Quic conn and send ALL it has
-        // loop {
-        //     match conn.send(&mut write_buf) {
-        //         Ok((write, _)) => match socket.send_to(&write_buf[..write], peer) {
-        //             Ok(sent) => {
-        //                 assert_eq!(write, sent);
-        //             }
-        //             Err(e) => {
-        //                 anyhow::bail!("Error sending packet socket: {:?}", e);
-        //             }
-        //         },
-        //         Err(quiche::Error::Done) => {
-        //             // No data, ok
-        //             break;
-        //         }
-        //         Err(e) => {
-        //             bail!("Error passing packet from Quic: {:?}", e);
-        //             //       break;
-        //         }
-        //     };
-        // }
 
         flush_send!(conn, socket, write_buf, peer);
 
-        if conn.is_established() && !connection_established {
-            connection_established = true;
-            stream_id = Some(2); //  Client initiated uni unistream
-            tracing::info!("Handshake completed, open stream #{}", stream_id.unwrap());
+        if conn.is_established() && stream_id.is_none() {
+            stream_id = Some(2); //  "2" is the first number for client initiated uni unistream
+            tracing::info!("Handshake completed ");
         }
 
         // Main send messages loop. Publisher spin here
-        if connection_established && message_count < MAX_MESSAGE_NUM {
-            if let Some(stream) = stream_id {
-                let mut offset = 0;
+        if stream_id.is_some() && message_count < MAX_MESSAGE_NUM {
+            let stream_id = stream_id.unwrap();
+            let mut offset = 0;
 
-                data_to_send[0..8].copy_from_slice(&MAGIC_NUMBER.to_be_bytes());
-                data_to_send[8..16].copy_from_slice(&now_ms().to_be_bytes());
+            data_to_send[0..8].copy_from_slice(&MAGIC_NUMBER.to_be_bytes());
+            data_to_send[8..16].copy_from_slice(&now_ms().to_be_bytes());
 
-                let total_size = data_to_send.len();
+            let total_size = data_to_send.len();
 
-                let moment = Instant::now();
+            let moment = Instant::now();
 
-                while offset < total_size {
-                    if let Some(to) = conn.timeout() {
-                        tracing::info!("conn.timeout() {:?}", to);
-                        timeout_instant = Instant::now() + to;
+            while offset < total_size {
+                if let Some(to) = conn.timeout() {
+                    timeout_instant = Instant::now() + to;
+                }
+
+                match conn.stream_send(stream_id, &data_to_send[offset..], false) {
+                    Ok(written) => {
+                        offset += written;
                     }
-                    // conn.stream_send -> conn.send -> socket.send_to
-                    match conn.stream_send(stream, &data_to_send[offset..], false) {
-                        Ok(written) => {
-                            offset += written;
-
-                            // tracing::info!(
-                            //     "Sent  {written} bytes into stream {stream} {total_size} {offset}",
-                            // );
-
-                            // Create datagrams
-                            // loop {
-                            //     match conn.send(&mut write_buf) {
-                            //         Ok((write, _)) => {
-                            //             socket.send_to(&write_buf[..write], peer)?;
-                            //         }
-                            //         Err(quiche::Error::Done) => {
-                            //             // Ok. There is no more work to do.
-                            //             break;
-                            //         }
-                            //         Err(e) => {
-                            //             bail!("Error to create quic packet: {:?}", e);
-                            //         }
-                            //     }
-                            // }
-
-                            //chores!(conn, socket, write_buf, peer);
-                        }
-                        Err(quiche::Error::Done) => {
-                            flush_send!(conn, socket, write_buf, peer);
-
-                            tokio::select! {
-
-                                result =socket.recv_from(&mut read_buf) =>  {
-
-                                    match result {
-                                        Ok((len, from)) => {
-                                            match conn.recv(
-                                                &mut read_buf[..len],
-                                                RecvInfo {
-                                                    from,
-                                                    to: socket.local_addr()?,
-                                                },
-                                            ) {
-                                                Ok(_) => {}
-                                                Err(e) => {
-                                                    anyhow::bail!("Error reading from quic conn: {:?}", e);
-                                                }
-                                            }
-                                        }
-                                        Err(e) => {
-                                            if e.kind() == io::ErrorKind::WouldBlock {
-                                                // Ok, no data
-                                                tokio::task::yield_now().await;
-                                            } else {
-                                                tracing::error!("Error reading packet from socket: {e}",);
-                                            }
-                                        }
+                    Err(quiche::Error::Done) => {
+                        flush_send!(conn, socket, write_buf, peer);
+                        tokio::select! {
+                            result = socket.recv_from(&mut read_buf) =>  {
+                                match result {
+                                    Ok((len, from)) => {
+                                        conn.recv(&mut read_buf[..len],RecvInfo {from,to: socket.local_addr()?})?;
                                     }
-                                }
-
-                                _ = sleep_until(timeout_instant) => {
-                                   // timeout_instant= Instant::now() + Duration::from_secs(1000);
-                                   tracing::info!("Called on_timeout()");
-                                    conn.on_timeout();
-
+                                    Err(e) if e.kind() == io::ErrorKind::WouldBlock  => {},
+                                    Err(e) => tracing::error!("Error reading packet from socket: {e}")
                                 }
                             }
-
-                            // //+++ Read from Quic com and send ALL it has
-                            // loop {
-                            //     match conn.send(&mut write_buf) {
-                            //         Ok((write, _)) => match socket
-                            //             .send_to(&write_buf[..write], peer)
-                            //         {
-                            //             Ok(sent) => {
-                            //                 assert_eq!(write, sent);
-                            //             }
-                            //             Err(e) => {
-                            //                 anyhow::bail!("Error sending packet socket: {:?}", e);
-                            //             }
-                            //         },
-                            //         Err(quiche::Error::Done) => {
-                            //             // No data, ok
-                            //             break;
-                            //         }
-                            //         Err(e) => {
-                            //             anyhow::bail!("Error passing packet from Quic: {:?}", e);
-                            //         }
-                            //     };
-                            // }
-                            // //---
-                        }
-                        Err(e) => {
-                            bail!("Error to send data to stream: {:?}", e);
+                            _ = sleep_until(timeout_instant) => {
+                                tracing::info!("Called on_timeout()");
+                                conn.on_timeout();
+                            }
                         }
                     }
-                    flush_send!(conn, socket, write_buf, peer);
+                    Err(e) => bail!("Error to send data to stream: {:?}", e),
                 }
+                tokio::task::yield_now().await;
+                flush_send!(conn, socket, write_buf, peer);
+            } // -- while offset < total_size 
 
-                message_count += 1;
+            message_count += 1;
 
-                let elapsed = moment.elapsed().as_millis() as u64;
-                if elapsed < 330 {
-                    // tokio::time::sleep(Duration::from_millis(330 - elapsed)).await;
-                } else {
-                    tracing::error!("Elapsed time is too long: {} ms", elapsed);
-                }
-                tokio::time::sleep(Duration::from_millis(1)).await;
-
-                tracing::info!("{message_count}");
-                let stats = conn.stats();
-                tracing::info!("{:?}", stats);
+            let elapsed = moment.elapsed().as_millis() as u64;
+            if elapsed < 330 {
+                // tokio::time::sleep(Duration::from_millis(330 - elapsed)).await;
+            } else {
+                tracing::error!("Elapsed time is too long: {} ms", elapsed);
             }
-            tokio::task::yield_now().await;
+            tracing::info!("Messages sent: {}, Stats{:?}", message_count, conn.stats());
         }
     }
     tracing::info!("Connection closed");
--- src/subscriber.rs
+++ src/subscriber.rs
@@ -1,143 +1,109 @@
 use anyhow::Result;
-
-use crate::config::BLOCK_SIZE;
-use crate::metrics::Metrics;
-use crate::quic_config::configure_server;
-use crate::{MAGIC_NUMBER, flush_send, now_ms};
 use quiche::{ConnectionId, Header, RecvInfo};
 use ring::rand::{SecureRandom, SystemRandom};
-use std::io;
-use std::sync::Arc;
-use std::time::Duration;
-use tokio::net::UdpSocket;
-use tokio::time::{Instant, sleep_until};
+use std::{io, sync::Arc, time::Duration};
+use tokio::{
+    net::UdpSocket,
+    time::{Instant, sleep_until},
+};
+
+use crate::{
+    MAGIC_NUMBER, config::BLOCK_SIZE, flush_send, metrics::Metrics, now_ms,
+    quic_config::configure_server,
+};
 
 pub async fn run(ot_metrics: Arc<Metrics>, address: String, port: u16) -> Result<()> {
-    let socket_address = format!("{}:{}", address, port);
+    let socket = UdpSocket::bind(format!("{address}:{port}")).await?;
+    tracing::info!("Server started on: {address}:{port}");
 
-    tracing::info!("Server started on: {}", socket_address);
-
-    let socket = UdpSocket::bind(socket_address).await?;
-    // socket.set_nonblocking(true)?;
-
+    let mut config = configure_server()?;
     let rng = SystemRandom::new();
 
-    let mut active_connections: Option<(String, quiche::Connection)> = None;
+    let mut conn_opt: Option<(String, quiche::Connection)> = None;
 
     let mut read_buf = [0; 65535];
     let mut write_buf = [0; 65535];
-
-    let mut config = configure_server()?;
-
     let mut block_aggregator = Vec::with_capacity(BLOCK_SIZE * 2);
-
-    let mut timeout_instant: Instant = Instant::now() + Duration::from_secs(10);
+    let mut timeout_instant = Instant::now() + Duration::from_secs(10);
 
     loop {
         tokio::select! {
-            result =socket.recv_from(&mut read_buf) =>  {
-                match  result {
+            result = socket.recv_from(&mut read_buf) => {
+                match result {
                     Ok((len, peer_addr)) => {
-                        let recv_info = RecvInfo {
-                            from: peer_addr,
-                            to: socket.local_addr()?,
-                        };
+                        let recv_info = RecvInfo { from: peer_addr, to: socket.local_addr()? };
+                        let header = Header::from_slice(&mut read_buf[..len], quiche::MAX_CONN_ID_LEN)?;
+                        let client_id = peer_addr.to_string();
 
-                        let header =  Header::from_slice(&mut read_buf[..len], quiche::MAX_CONN_ID_LEN)?;
+                        match &mut conn_opt {
+                            Some((_id, conn)) => {
+                                conn.recv(&mut read_buf[..len], recv_info)?;
+                                flush_send!(conn, socket, write_buf, peer_addr);
+                            }
+                            None if header.ty == quiche::Type::Initial => {
+                                tracing::info!("New connection: {client_id}");
 
-                        let client_addr = peer_addr.to_string();
+                                let scid = {
+                                    let mut id = [0; quiche::MAX_CONN_ID_LEN];
+                                    rng.fill(&mut id).unwrap();
+                                    ConnectionId::from_vec(id.to_vec())
+                                };
 
-                        // Connection already exists
-                        if  let Some((_, conn)) = &mut active_connections {
-                            conn.recv(&mut read_buf[..len], recv_info) ?;
-                            flush_send!(conn, socket, write_buf, peer_addr);
+                                let mut conn = quiche::accept(&scid, None, recv_info.to, peer_addr, &mut config)?;
+                                conn.recv(&mut read_buf[..len], recv_info)?;
+                                flush_send!(conn, socket, write_buf, peer_addr);
 
-                        } else if header.ty == quiche::Type::Initial {
-                            tracing::info!("New connection {}", client_addr);
-                            let rand_id = {
-                                let mut rand_id = [0; quiche::MAX_CONN_ID_LEN];
-                                rng.fill(&mut rand_id).unwrap();
-                                rand_id
-                            };
-                            let scid = ConnectionId::from_ref(&rand_id);
-
-                            let mut conn = quiche::accept(&scid, None, recv_info.to, peer_addr, &mut config)?;
-
-                            conn.recv(&mut read_buf[..len], recv_info)?;
-                            flush_send!(conn, socket, write_buf, peer_addr);
-                            active_connections = Some((client_addr.clone(),conn));
+                                conn_opt = Some((client_id, conn));
+                            }
+                            _ => {}
                         }
                     }
-                    Err(e) => {
-                        if e.kind() == io::ErrorKind::WouldBlock {
-                            // No data, that;s ok
-                        } else {
-                            tracing::error!("Error: {:?}", e);
-                        }
-                    }
+                    Err(e) if e.kind() == io::ErrorKind::WouldBlock => {}
+                    Err(e) => tracing::error!("recv_from error: {e:?}"),
                 }
             }
-            _ = sleep_until(timeout_instant) => {
-                if let Some((_key,   conn)) = &mut  active_connections {
-                        conn.on_timeout();
-                        tracing::info!("Called on_timeout!");
 
-                };
+            _ = sleep_until(timeout_instant) => {
+                if let Some((_id, conn)) = &mut conn_opt {
+                    conn.on_timeout();
+                    tracing::info!("Timeout triggered");
+                }
             }
         }
 
-        if let Some((client_addr, conn)) = &mut active_connections {
+        if let Some((client_id, conn)) = &mut conn_opt {
             if conn.is_established() {
-                // for simplicity and performance we have only one stream per connection in this test
                 if let Some(stream_id) = conn.readable().next() {
                     let mut stream_buf = vec![0; BLOCK_SIZE * 2];
-
                     match conn.stream_recv(stream_id, &mut stream_buf) {
-                        Ok((read, _fin)) => {
-                            if read > 0 {
-                                block_aggregator.extend_from_slice(&stream_buf[..read]);
+                        Ok((read, _fin)) if read > 0 => {
+                            block_aggregator.extend_from_slice(&stream_buf[..read]);
 
-                                while block_aggregator.len() >= BLOCK_SIZE {
-                                    let block =
-                                        block_aggregator.drain(..BLOCK_SIZE).collect::<Vec<u8>>();
+                            while block_aggregator.len() >= BLOCK_SIZE {
+                                let block =
+                                    block_aggregator.drain(..BLOCK_SIZE).collect::<Vec<u8>>();
 
-                                    let magic_bytes = &block[0..8];
-                                    let magic_number: u64 =
-                                        u64::from_be_bytes(magic_bytes.try_into()?);
+                                let magic = u64::from_be_bytes(block[..8].try_into()?);
+                                assert_eq!(
+                                    magic, MAGIC_NUMBER,
+                                    "Protocol mismatch or stream restarted"
+                                );
 
-                                    assert_eq!(
-                                        magic_number, MAGIC_NUMBER,
-                                        "Quic guaranties that. Otherwise producer was restarted and Subscriber must be restarted too"
-                                    );
-
-                                    let timestamp_bytes = &block[8..16];
-                                    let sent_timestamp =
-                                        u64::from_be_bytes(timestamp_bytes.try_into()?);
-
-                                    let time_now = now_ms();
-                                    let latency = time_now - sent_timestamp;
-                                    tracing::info!(
-                                        "Latency ms: {} = {} - {}",
-                                        latency,
-                                        time_now,
-                                        sent_timestamp
-                                    );
-                                    ot_metrics.latency.record(latency, &[]);
-                                }
+                                let sent_ts = u64::from_be_bytes(block[8..16].try_into()?);
+                                let latency = now_ms() - sent_ts;
+                                ot_metrics.latency.record(latency, &[]);
+                                tracing::info!("Latency ms: {latency}");
                             }
                         }
-                        Err(quiche::Error::Done) => {
-                            // No data, ok
-                            tokio::task::yield_now().await;
-                        }
-                        Err(e) => {
-                            // It is not a big deal that we failed sending a datagram. this is Quic, it has delivery guaranties.
-                            tracing::error!("Errorreading from stream {}: {:?}", stream_id, e);
-                        }
+                        Err(quiche::Error::Done) => tokio::task::yield_now().await,
+                        Err(e) => tracing::error!("Stream recv error: {e:?}"),
+                        _ => {}
                     }
                 }
             }
-            flush_send!(conn, socket, write_buf, client_addr.clone());
+
+            flush_send!(conn, socket, write_buf, client_id.clone());
+
             if let Some(to) = conn.timeout() {
                 timeout_instant = Instant::now() + to;
             }
